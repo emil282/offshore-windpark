3da5567efb4bfe5039e0dbbf1d27064a4de0648f
Eva :) <evalangstein@freenet.de>
2023-11-22 16:52:27 +0100

    Berechnung der Energie durch Windgeschwindigkeit ergänzt

--- src/js/knob-view.js
+++ src/js/knob-view.js
@@ -1,10 +1,12 @@
 class KnobView {
-  constructor(config) {
+  constructor(config, tileCounterView) {
     this.config = config;
+    this.tileCounterView = tileCounterView;
     /*this.$element = $("<div></div>")
       .addClass("index-list")
       .append($("<div></div>").addClass("flex"));*/
 
+    // Creates two knobs
     this.knobs = [
       this.makeKnob(this.config.winddirection),
       this.makeKnob(this.config.windspeed),
@@ -26,20 +28,32 @@ class KnobView {
           '_span">0</span>'
       );
 
+    // Event Listeners for when the knobs change
     $(document).ready(() => {
       $(`#${this.config.winddirection.name}`).on("input", (event) => {
+        // Sets the current winddirection
         let value = (event.target.value % 1) * event.target.divisions;
         $(`#${event.currentTarget.id}_span`).html(
           this.config.winddirection.labels[value]
         );
+        // Calculate the energy gain
+        this.tileCounterView.handleUpdate();
       });
       $(`#${this.config.windspeed.name}`).on("input", (event) => {
+        // Sets the current windspeed
         let value = Math.round((event.target.value % 1) * 12);
         $(`#${event.currentTarget.id}_span`).html(value);
+        // Calculate the energy gain
+        this.tileCounterView.handleUpdate();
       });
     });
   }
 
+  /**
+   * Creates the html tags for a x-knob.
+   * @param {*} config
+   * @returns
+   */
   makeKnob(config) {
     let element = $("<div id='" + config.name + "'></div>").addClass("windDiv");
     let knob =
@@ -59,8 +73,14 @@ class KnobView {
     return $("<div></div>").addClass("flex").append(element).append(label);
   }
 
+  /**
+   * Creates a circular labeling around the knob
+   * @param {*} config
+   * @returns
+   */
   makeLabeling(config) {
     let element = $("<ul></ul>").addClass("circle-container");
+    // Get labeling from config
     config.labels.forEach((item) => {
       element.append($("<li><span>" + item + "</span></li>"));
     });
